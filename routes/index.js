2fba5eb365e13c0f6892bfcd140c5b1722ead6ac
ivan <lx1312315409@gmail.com>
2019-06-27 09:37:15 +0800

    ’‘

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
-
-
+var NodeRSA = require('node-rsa')
+var fs = require('fs')
+var crypto = require('crypto');
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
@@ -11,11 +12,19 @@ router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
 })
 
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+router.get('/set', async (ctx, next) => {
+    var key = new NodeRSA({ b: 512 })
+    key.setOptions({ encryptionScheme: 'pkcs1' })
+    var privatePem = key.exportKey('pkcs1-private-pem')
+    var publicPem = key.exportKey('pkcs1-public-pem')
+    fs.writeFile('./pem/public.pem', publicPem, (err) => {
+    if (err) throw err
+    console.log('公钥已保存！')
+    })
+    fs.writeFile('./pem/private.pem', privatePem, (err) => {
+    if (err) throw err
+    console.log('私钥已保存！')
+    })
 })
-
 
 module.exports = router
